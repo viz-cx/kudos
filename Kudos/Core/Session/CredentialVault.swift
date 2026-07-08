035cd3a9759e2067ec683004743facbe3ebe28d8
Vladimir Babin <vovababin@gmail.com>
2026-07-08 21:55:33 +0800

    feat: add CredentialVault (regular key only)

---
+++ Kudos/Core/Session/CredentialVault.swift
@@ -0,0 +1,23 @@
+import KeychainAccess
+
+actor CredentialVault {
+    private let keychain = Keychain(service: "cx.viz.kudos")
+        .accessibility(.whenPasscodeSetThisDeviceOnly, authenticationPolicy: [.biometryAny])
+        .synchronizable(true)
+
+    func load() -> Credentials? {
+        guard let username = try? keychain.getString("username"),
+              let key = try? keychain.getString("regularKey") else { return nil }
+        return Credentials(username: username, regularKeyWIF: key)
+    }
+
+    func save(_ credentials: Credentials) {
+        keychain["username"] = credentials.username
+        keychain["regularKey"] = credentials.regularKeyWIF
+    }
+
+    func clear() {
+        try? keychain.remove("username")
+        try? keychain.remove("regularKey")
+    }
+}
